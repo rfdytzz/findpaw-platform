7c7624544f4e2345ff555bc4773ed6cd0ff370e0
rfdytzz <rafkadyta8@gmail.com>
2026-07-13 22:01:47 +0700

    fix: Profile Layout

--- Frontend/src/components/ProfilSidebar.vue
+++ Frontend/src/components/ProfilSidebar.vue
@@ -17,14 +17,14 @@
             <li>
                 <a href="#"
                     class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-                    Billing
+                    Verified Account
                 </a>
             </li>
 
             <li>
                 <a href="#"
                     class="block rounded-lg px-4 py-2 text-sm font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-700">
-                    Invoices
+                    Bookmark
                 </a>
             </li>
 
--- Frontend/src/views/user/Profile.vue
+++ Frontend/src/views/user/Profile.vue
@@ -9,9 +9,15 @@ const toggleEditor = () => {
     isDisable.value = !isDisable.value
 }
 
+const editAvatar = ref(false)
+const toggleEditAvatar = () => {
+    editAvatar.value = !editAvatar.value
+}
+
 const name = ref('Rafka Dyta Pratama')
 const email = ref('rafka@mymart.id')
 const phone_number = ref('085775590136')
+const address = ref('Jl. Sakhuji RT 1/1, Desa Candinegara, Kec Pekuncen, Kab. Banyumas, Prov. Jateng')
 
 onMounted(() => {
     document.title = 'FindPaw - Profile'
@@ -59,36 +65,93 @@ onMounted(() => {
                     <div class="grid grid-cols-3 gap-5">
                         <div class="col-span-2 flex gap-5 flex-col">
                             <div class="flex flex-col gap-2">
-                                <label for="name" class="text-sm font-medium">Name <span
-                                        class="text-teal-600">*</span></label>
-                                <input v-model="name" type="text" :disabled="isDisable"
-                                    :class="!isDisable ? 'ring-gray-300' : ''"
-                                    class="focus:outline-0 ring p-2 text-sm focus:ring-teal-500 rounded-md ring-gray-200 shadow bg-gray-50 focus:bg-white"
-                                    name="" id="name">
+                                <label for="name">
+                                    <span class="text-sm font-medium text-gray-700"> Name <span
+                                            class="text-teal-600">*</span> </span>
+
+                                    <input v-model="name" type="text" id="name" :disabled="isDisable"
+                                        class="mt-0.5 w-full rounded focus:ring-teal-600 ring focus:outline-0 ring-gray-100 p-2 shadow-sm sm:text-sm" />
+                                </label>
                             </div>
                             <div class="flex items-center gap-5">
-                                <div class="flex flex-col gap-2 flex-1">
-                                    <label for="name" class="text-sm font-medium">Email <span
-                                            class="text-teal-600">*</span></label>
-                                    <input v-model="email" type="text" disabled
-                                        class="focus:outline-0 rounded-md text-gray-700 p-2 text-sm shadow bg-gray-200 focus:bg-white"
-                                        name="" id="name">
+                                <label for="email" class="flex-1">
+                                    <span class="text-sm font-medium text-gray-700"> Email <span
+                                            class="text-teal-600">*</span> </span>
+
+                                    <input v-model="email" type="email" id="email" disabled
+                                        class="mt-0.5 w-full rounded focus:ring-teal-600 ring focus:outline-0 ring-gray-100 p-2 shadow-sm sm:text-sm" />
+                                </label>
+                                <label for="phone_number" class="flex-1">
+                                    <span class="text-sm font-medium text-gray-700"> Phone Number <span
+                                            class="text-teal-600">*</span> </span>
+
+                                    <input v-model="phone_number" type="tel" id="phone_number" :disabled="isDisable"
+                                        class="mt-0.5 w-full rounded focus:ring-teal-600 ring focus:outline-0 ring-gray-100 p-2 shadow-sm sm:text-sm" />
+                                </label>
+                            </div>
+                            <div class="flex items-center gap-5">
+                                <label for="gender" class="">
+                                    <span class="text-sm font-medium text-gray-700"> Gender <span
+                                            class="text-teal-600">*</span> </span>
+
+                                    <select name="" :disabled="isDisable"
+                                        class="mt-0.5 w-full rounded focus:ring-teal-600 ring focus:outline-0 ring-gray-100 p-2 shadow-sm sm:text-sm"
+                                        id="gender">
+                                        <option disabled>Select</option>
+                                        <option value=""><i class='bx bx-male-sign'></i>Male</option>
+                                        <option value="">Female</option>
+                                    </select>
+                                </label>
+                                <label for="phone_number" class="flex-1">
+                                    <span class="text-sm font-medium text-gray-700"> Address <span
+                                            class="text-teal-600">*</span> </span>
+
+                                    <input v-model="address" type="text" id="address" :disabled="isDisable"
+                                        class="mt-0.5 w-full rounded focus:ring-teal-600 ring focus:outline-0 ring-gray-100 p-2 shadow-sm sm:text-sm" />
+                                </label>
+                            </div>
+                        </div>
+                        <div class="col-span-1 flex flex-col relative">
+                            <div class="justify-center flex">
+                                <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=format&fit=crop&q=80&w=1160"
+                                    alt="" class="size-40 rounded-full object-cover" />
+                            </div>
+                            <div class="justify-end flex flex-col flex-1 absolute right-15 top-0">
+                                <div @click="toggleEditAvatar"
+                                    class="flex items-center cursor-pointer gap-2 px-2 py-1 hover:bg-gray-200 bg-gray-100 rounded-md ring ring-gray-200">
+                                    <i class="bx bx-edit-alt"></i>Edit
                                 </div>
-                                <div class="flex flex-col gap-2 flex-1">
-                                    <label for="name" class="text-sm font-medium">Phone Number <span
-                                            class="text-teal-600">*</span></label>
-                                    <input v-model="phone_number" type="text" :disabled="isDisable"
-                                        :class="!isDisable ? 'ring-gray-300' : ''"
-                                        class="focus:outline-0 ring p-2 text-sm focus:ring-teal-500 rounded-md ring-gray-200 shadow bg-gray-50 focus:bg-white"
-                                        name="" id="name">
+                                <div role="menu" :class="editAvatar ? 'block' : 'hidden'"
+                                    class="absolute inset-e-0 top-12 z-auto w-31 divide-y divide-gray-200 overflow-hidden rounded border border-gray-300 bg-white shadow-sm">
+                                    <div>
+                                        <label for="avatar"
+                                            class="block px-3 py-2 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900"
+                                            role="menuitem">
+                                            Change Avatar
+                                            <input type="file" name="" id="avatar" hidden>
+                                        </label>
+
+                                        <a href="#"
+                                            class="block px-3 py-2 text-sm font-medium text-gray-700 transition-colors hover:bg-gray-50 hover:text-gray-900"
+                                            role="menuitem">
+                                            Remove Avatar
+                                        </a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-span-1 flex flex-col items-center gap-2">
-                            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=format&fit=crop&q=80&w=1160"
-                                alt="" class="size-40 rounded-full object-cover" />
-                            <button class="flex items-center gap-2 px-2 py-1 hover:bg-gray-200g bg-gray-100 rounded-md ring ring-gray-200"><i class="bx bx-edit-alt"></i>Edit</button>
-                        </div>
+                    </div>
+                    <div v-if="!isDisable" class="flex mt-7 items-center gap-2">
+                        <button
+                            class="inline-block rounded border border-teal-600 bg-teal-600 px-2 py-1 font-medium text-white shadow-sm transition-colors hover:bg-teal-700"
+                            href="#">
+                            Save Changes
+                        </button>
+                        <button @click="toggleEditor"
+                            class="inline-block rounded border border-teal-600 bg-white-600 px-2 py-1 font-medium text-teal-600 shadow-sm transition-colors hover:bg-gray-100"
+                            href="#">
+                            Cancel
+                        </button>
                     </div>
                 </form>
             </div>
