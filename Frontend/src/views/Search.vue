ae92ac815a0c44a189df28ee8a86d738c294aa5a
rfdytzz <rafkadyta8@gmail.com>
2026-07-16 21:19:19 +0700

    fix: dummy data

--- Frontend/src/views/Search.vue
+++ Frontend/src/views/Search.vue
@@ -13,7 +13,7 @@ import ProductCards from '@/components/ProductCards.vue';
                 <details class="group relative overflow-hidden rounded border border-gray-300 shadow-sm">
                     <summary
                         class="flex items-center justify-between gap-2 p-3 text-gray-700 transition-colors hover:text-gray-900 [&::-webkit-details-marker]:hidden">
-                        <span class="text-sm font-medium"> Availability </span>
+                        <span class="text-sm font-medium"> Category </span>
 
                         <span class="transition-transform group-open:-rotate-180">
                             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -41,21 +41,21 @@ import ProductCards from '@/components/ProductCards.vue';
                                     <input type="checkbox" class="size-5 rounded border-gray-300 shadow-sm"
                                         id="Option1" />
 
-                                    <span class="text-sm font-medium text-gray-700"> Option 1 </span>
+                                    <span class="text-sm font-medium text-gray-700"> Cat </span>
                                 </label>
 
                                 <label for="Option2" class="inline-flex items-center gap-3">
                                     <input type="checkbox" class="size-5 rounded border-gray-300 shadow-sm"
                                         id="Option2" />
 
-                                    <span class="text-sm font-medium text-gray-700"> Option 2 </span>
+                                    <span class="text-sm font-medium text-gray-700"> Dog </span>
                                 </label>
 
                                 <label for="Option3" class="inline-flex items-center gap-3">
                                     <input type="checkbox" class="size-5 rounded border-gray-300 shadow-sm"
                                         id="Option3" />
 
-                                    <span class="text-sm font-medium text-gray-700"> Option 3 </span>
+                                    <span class="text-sm font-medium text-gray-700"> Hamsters </span>
                                 </label>
                             </div>
                         </fieldset>
@@ -90,14 +90,14 @@ import ProductCards from '@/components/ProductCards.vue';
                                 <span class="text-sm text-gray-700"> Min </span>
 
                                 <input type="number" id="MinPrice" value="0"
-                                    class="mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm" />
+                                    class="mt-0.5 focus:outline-0 w-full rounded border-gray-300 shadow-sm sm:text-sm" />
                             </label>
 
                             <label for="MaxPrice">
                                 <span class="text-sm text-gray-700"> Max </span>
 
                                 <input type="number" id="MaxPrice" value="600"
-                                    class="mt-0.5 w-full rounded border-gray-300 shadow-sm sm:text-sm" />
+                                    class="mt-0.5 focus:outline-0 w-full rounded border-gray-300 shadow-sm sm:text-sm" />
                             </label>
                         </div>
                     </div>
@@ -105,7 +105,13 @@ import ProductCards from '@/components/ProductCards.vue';
             </div>
         </div>
         <div class="col-span-4 h-screen">
-            <div class="font-medium">Result for: Cat</div>
+            <div class="flex items-center justify-between">
+                <div class="font-medium">Result for: Cat</div>
+                <div class="flex items-center gap-2">
+                    <div class="text-sm flex items-center gap-1 bg-teal-600/20 border border-teal-600 px-1.5 py-1 rounded-2xl text-teal-600"><i class="bx bx-x"></i>Cat</div>
+                    <div class="text-sm flex items-center gap-1 bg-teal-600/20 border border-teal-600 px-1.5 py-1 rounded-2xl text-teal-600"><i class="bx bx-x"></i>1-2 y/o</div>
+                </div>
+            </div>
             <div class="grid grid-cols-5 gap-5 mt-5">
                 <ProductCards />
                 <ProductCards />
